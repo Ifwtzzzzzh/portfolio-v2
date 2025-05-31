7093659ba3a390d3475bb25fe105e82829f61a63
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2025-05-31 12:54:41 +0700

    style: styling hero 3d model

--- resources/js/pages/model/HeroLights.jsx
+++ resources/js/pages/model/HeroLights.jsx
@@ -6,12 +6,12 @@ const HeroLights = () => {
             <spotLight position={[4, 5, 4]} angle={0.3} penumbra={0.5} intensity={40} color="#4CC9F0" />
             <spotLight position={[-3, 5, 5]} angle={0.4} penumbra={1} intensity={60} color="#9D4EDD" />
             <primitive
-                object={new THREE.RectAreaLight('#a259ff', 8, 3, 2)}
+                object={new THREE.RectAreaLight('#A259FF', 8, 3, 2)}
                 position={[1, 3, 4]}
                 rotation={[-Math.PI / 4, Math.PI / 4, 0]}
                 intensity={15}
             />
-            <pointLight position={[0, 1, 0]} intensity={10} color="#7209b7" />
+            <pointLight position={[0, 1, 0]} intensity={10} color="#7209B7" />
         </>
     );
 };
