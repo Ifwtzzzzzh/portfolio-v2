247a1062a28d6207251dc60fd95f2b2311aa9863
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2025-06-02 21:25:16 +0700

    chore: on progress styling navbar #4

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,9 +1,11 @@
+import Navbar from './components/NavBar.jsx';
 import Hero from './sections/Hero.jsx';
 import ShowcaseSection from './sections/ShowcaseSection.jsx';
 
 const App = () => {
     return (
         <>
+            <Navbar />
             <Hero />
             <ShowcaseSection />
         </>
