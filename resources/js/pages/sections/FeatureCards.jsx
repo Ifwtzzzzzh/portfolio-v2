2af4a45954165ea703b7ac8689d72cf371db601d
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2025-06-05 21:23:51 +0700

    style: feature cards #6

---
+++ resources/js/pages/sections/FeatureCards.jsx
@@ -0,0 +1,19 @@
+import { abilities } from '../constants/index.js';
+
+const FeatureCards = () => (
+    <div className="padding-x-lg w-full">
+        <div className="grid-3-cols mx-auto">
+            {abilities.map(({ imgPath, title, desc }) => (
+                <div key={title} className="card-border flex flex-col gap-4 rounded-xl p-8">
+                    <div className="flex size-14 items-center justify-center rounded-full">
+                        <img src={imgPath} alt={title} />
+                    </div>
+                    <h3 className="mt-2 text-2xl font-semibold text-white">{title}</h3>
+                    <p className="text-white-50 text-lg">{desc}</p>
+                </div>
+            ))}
+        </div>
+    </div>
+);
+
+export default FeatureCards;
