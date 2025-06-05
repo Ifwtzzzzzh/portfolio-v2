12065a4d409afa1ae6ea5d49fa0266c174579629
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2025-06-05 21:12:52 +0700

    style: styling logo showcase #5

---
+++ resources/js/pages/components/LogoSection.jsx
@@ -0,0 +1,39 @@
+import { logoIconsList } from '../constants/index.js';
+
+const LogoIcon = ({ icon }) => {
+    return (
+        <div className="flex-center marquee-item flex-none">
+            <img src={icon.imgPath} alt={icon.name} />
+        </div>
+    );
+};
+
+const LogoSection = () => {
+    return (
+        <div className="relative my-10 md:my-20">
+            <div className="gradient-edge" />
+            <div className="gradient-edge" />
+            <div className="marquee h-52">
+                <div className="marquee-box gap-5 md:gap-12">
+                    {logoIconsList.map((icon) => (
+                        <LogoIcon key={icon.name} icon={icon} />
+                    ))}
+                    {logoIconsList.map((icon) => (
+                        <LogoIcon key={icon.name} icon={icon} />
+                    ))}
+                    {logoIconsList.map((icon) => (
+                        <LogoIcon key={icon.name} icon={icon} />
+                    ))}
+                    {logoIconsList.map((icon) => (
+                        <LogoIcon key={icon.name} icon={icon} />
+                    ))}
+                    {logoIconsList.map((icon) => (
+                        <LogoIcon key={icon.name} icon={icon} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LogoSection;
