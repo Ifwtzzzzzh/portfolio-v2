9682b1a7247835de00ccde5a9dbabcc494a555b2
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2025-06-24 20:08:40 +0700

    feat: add testimonials

---
+++ resources/js/pages/sections/Testimonials.jsx
@@ -0,0 +1,31 @@
+import GlowCard from '../components/GlowCard';
+import TitleHeader from '../components/TitleHeader';
+import { testimonials } from '../constants';
+
+const Testimonials = () => {
+    return (
+        <section id="testimonials" className="flex-center section-padding">
+            <div className="h-full w-full px-5 md:px-10">
+                <TitleHeader title="What People Say About Me?" sub="⭐️ Customer feedback highlights" />
+
+                <div className="mt-16 columns-1 md:columns-2 lg:columns-3">
+                    {testimonials.map((testimonial, index) => (
+                        <GlowCard card={testimonial} key={index} index={index}>
+                            <div className="flex items-center gap-3">
+                                <div>
+                                    <img src={testimonial.imgPath} alt="" />
+                                </div>
+                                <div>
+                                    <p className="font-bold">{testimonial.name}</p>
+                                    <p className="text-white-50">{testimonial.mentions}</p>
+                                </div>
+                            </div>
+                        </GlowCard>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Testimonials;
--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -5,6 +5,7 @@ import FeatureCards from './sections/FeatureCards.jsx';
 import Hero from './sections/Hero.jsx';
 import ShowcaseSection from './sections/ShowcaseSection.jsx';
 import TechStack from './sections/TechStack.jsx';
+import Testimonials from './sections/Testimonials.jsx';
 
 const App = () => {
     return (
@@ -16,6 +17,7 @@ const App = () => {
             <FeatureCards />
             <ExperienceSection />
             <TechStack />
+            <Testimonials />
         </>
     );
 };
